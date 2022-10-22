4c6523d527b6a77b901cbf6b9ef0ba389b7791ce
Alex Rogna <alexrogna@yahoo.com>
2022-10-22 16:12:45 +0100

    Add Normal items tests

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,25 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  items = [
+    Item.new(name="Normal item", sell_in=1, quality=10),
+    Item.new(name="Normal item", sell_in=0, quality=20),
+    Item.new(name="Normal item", sell_in=-1, quality=1),
+  ]
+  GildedRose.new(items).update_quality()
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    # Normal item tests
+    it "Normal items quality decrese by 1 if sell_in > 0" do
+      expect(items[0].quality).to eq 9
     end
+    it "Normal items quality decrese by 2 if sell_in <= 0" do
+      expect(items[1].quality).to eq 18
+    end
+    it "Normal items quality decrese by 2 if sell_in <= 0 and the quality doesn't become negative" do
+      expect(items[2].quality).to eq 0
+    end
+
   end
 
 end
