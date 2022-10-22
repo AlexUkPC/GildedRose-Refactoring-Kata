a3c493fee4e743b88407e86760c5b22960e0d283
Alex Rogna <alexrogna@yahoo.com>
2022-10-22 17:02:55 +0100

    combine ifs statements

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -9,25 +9,19 @@ class GildedRose
       item.quality = item.quality + 1
     end
     item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
-      if item.quality < 50
-        item.quality = item.quality + 1
-      end
+    if item.sell_in < 0 && item.quality < 50
+      item.quality = item.quality + 1
     end
   end
 
   def updateBackstage(item)
     if item.quality < 50
       item.quality = item.quality + 1
-      if item.sell_in < 11
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
+      if item.sell_in < 11 && item.quality < 50
+        item.quality = item.quality + 1
       end
-      if item.sell_in < 6
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
+      if item.sell_in < 6 && item.quality < 50
+        item.quality = item.quality + 1
       end
     end
     item.sell_in = item.sell_in - 1
@@ -45,10 +39,8 @@ class GildedRose
       item.quality = item.quality - 1
     end
     item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
-      if item.quality > 0
-        item.quality = item.quality - 1
-      end
+    if item.sell_in < 0 && item.quality > 0
+      item.quality = item.quality - 1
     end
   end
 
