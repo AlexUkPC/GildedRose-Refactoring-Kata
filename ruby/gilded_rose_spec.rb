bf17bc4ba31ffabca8d38c5842e9238c3cec891a
Alex Rogna <alexrogna@yahoo.com>
2022-10-22 16:21:31 +0100

    Add tests for Sulfuras

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -7,7 +7,9 @@ describe GildedRose do
     Item.new(name="Normal item", sell_in=-1, quality=1),
     Item.new(name="Aged Brie", sell_in=2, quality=0),
     Item.new(name="Aged Brie", sell_in=0, quality=10),
-    Item.new(name="Aged Brie", sell_in=-1, quality=49)
+    Item.new(name="Aged Brie", sell_in=-1, quality=49),
+    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=1, quality=80),
+    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)
   ]
   GildedRose.new(items).update_quality()
 
@@ -31,6 +33,13 @@ describe GildedRose do
     end
     it "Aged Brie quality increases by 2 if sell_in <= 0 and the quality increase above 50" do
       expect(items[5].quality).to eq 50
+    end
+    # Sulfuras tests
+    it "Sulfuras quality doesn't change" do
+      expect(items[6].quality).to eq 80
+    end
+    it "Sulfuras quantity doesn't change" do
+      expect(items[7].sell_in).to eq -1
     end
   end
 
